dd546ae3532906af4bc122774023e1eaf98c8e05
e-c-e-m <ecem.n.ozturk@gmail.com>
2021-09-13 00:13:42 +0100

    Uploading repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.scss";
+import CartoonPortrait from "./assets/images/anime2.png";
+import Parallax from "react-rellax";
+import Project from "./components/Project";
 
 function App() {
+  const [scroll, setScroll] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 50) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="leftSide">
+          <Parallax speed={7}>
+            <h1 className="homeTitle">Hey, I'm Ecem.</h1>
+          </Parallax>
+          <Parallax speed={5}>
+            <p className="homeText">(pronounced eh-jem)</p>
+          </Parallax>
+          <Parallax speed={5}>
+            <p className="homeText">I'm a frontend developer.</p>
+          </Parallax>
+          <Parallax speed={3}>
+            <p className="homeText">
+              Here's the part where I should put something witty - but I got
+              nothing.
+            </p>
+          </Parallax>
+          <Parallax speed={1}>
+            <p className={scroll ? "techActive" : "tech"}>
+              REACT // NEXT.JS // HTML // CSS // JAVASCRIPT // NODE.JS
+            </p>
+          </Parallax>
+          <div className="homeBttnLayout">
+            <button className="homeBttn">Email</button>
+            <button className="homeBttn">CV</button>
+          </div>
+        </div>
+        <div className="rightSide">
+          <Parallax speed={-3}>
+            <img src={CartoonPortrait} alt="illustration" className="mainImg" />
+          </Parallax>
+        </div>
+      </div>
+      <div className="portfolioSection">
+        <h1>Projects</h1>
+        <Project />
+        <Project />
+        <Project />
+      </div>
+      <div className="footer">
+        <p>Copyright © 2021 | Made with ♡ by Ecem</p>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Project/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./project.scss";
+
+const Project = () => {
+  return <div className="projectBox"></div>;
+};
+
+export default Project;
