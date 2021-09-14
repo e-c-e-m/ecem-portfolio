dbdbb7bd856af4521bd53bc1a606c91c363c6f20
e-c-e-m <ecem.n.ozturk@gmail.com>
2021-09-14 23:18:37 +0100

    Added new pic and json change

--- src/components/RightSide/index.js
+++ src/components/RightSide/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Parallax from "react-rellax";
-import CartoonPortrait from "../../assets/images/anime2.png";
+import Portrait from "../../assets/images/portrait.jpeg";
 import "./rightside.scss";
 
 const RightSide = () => {
   return (
     <div className="rightSide">
       <Parallax speed={-3}>
-        <img src={CartoonPortrait} alt="illustration" className="mainImg" />
+        <img src={Portrait} alt="illustration" className="mainImg" />
       </Parallax>
     </div>
   );
